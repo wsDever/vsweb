d74ff72006c8eb3a7f050646350f6c67502ebd76
wushang <wushang@tfzq.com>
2021-08-12 15:43:32 +0800

    首次提交

---
+++ src/url.js
@@ -0,0 +1,12 @@
+var urlLists = [
+    {
+        "name": "A",
+        "url": "aaa"
+    },{
+        "name": "百度",
+        "url": "https://baidu.com"
+    },{
+        "name": "微信读书",
+        "url": "https://weread.qq.com"
+    }
+]
